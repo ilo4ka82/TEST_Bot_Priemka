46cab56ed491b0d8f061661f8830420bb3abec3c
ilo4ka82 <chesslove97@gmail.com>
2025-06-17 16:56:41 +0300

    зависимости

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 TELEGRAM_BOT_TOKEN = "8077716714:AAFjWQjHKN5_gtXr12xwWu2JW1qIdBPksk8"
 ADMIN_TELEGRAM_IDS = [1125354576, 511913237]
 DATABASE_NAME = "time_tracker.db"
@@ -13,8 +12,6 @@ SECTOR_WEEKLY_NORMS = {
     "СС": 40,  
     "ВИ": 40,  
     "ОП": 40,
-    # Важно, чтобы ключи (названия секторов) точно совпадали с тем, 
-    # как они хранятся в поле application_department таблицы users.
     "DEFAULT_NORM": 40 
 }
 
