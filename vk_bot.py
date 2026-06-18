34725a19f5c132dc80b72bd251b18006c397f666
Иванов Андрей Евгеньевич (ОПК-А) <a.e.ivanov@mtuci.adm>
2026-06-18 19:43:15 +0300

    boevoi

--- vk_bot.py
+++ vk_bot.py
@@ -24,7 +24,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
-logging.getLogger("vkbottle").setLevel(logging.DEBUG)
+logging.getLogger("vkbottle").setLevel(logging.WARNING)
 
 MOSCOW_TZ = pytz.timezone("Europe/Moscow")
 
